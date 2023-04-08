2cccf93b9000b4e81a94e8479a5e1a9da7609093
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-04-08 19:07:51 -0400

    Added script in JS for calculate and tip button

---
+++ index.js
@@ -0,0 +1,37 @@
+const calculateButton = document.querySelector(".calculateBtn");
+calculateButton.addEventListener("click", calculateAmount);
+
+const tipButton = document.querySelector(".addTip");
+tipButton.addEventListener("click", showTip);
+
+function showTip(event) {
+	event.preventDefault();
+	tip.style.display = "block";
+	console.log(showTip)
+}
+
+function calculateAmount(event) {
+	event.preventDefault();
+	const billInput = document.querySelector(".billInput").value;
+	const numberOfPeopleInput = document.querySelector(".numberOfPeopleInput").value;
+	const tipInput = document.querySelector("#tip").value;
+
+	if (billInput === "" || numberOfPeopleInput === "" || billInput < 1 || numberOfPeopleInput < 1){
+		Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Please, put the correct information!",
+    });
+	}
+	let amountPerPerson = billInput/numberOfPeopleInput;
+	let tipPerPerson = (billInput * tipInput) / numberOfPeopleInput;
+	let totalSum = amountPerPerson + tipPerPerson;
+
+	amountPerPerson = amountPerPerson.toFixed(2);
+	tipPerPerson = tipPerPerson.toFixed(2);
+	totalSum = totalSum.toFixed(2);
+
+	document.querySelector(".dividedBill").textContent = amountPerPerson;
+	document.querySelector(".dividedTip").textContent = tipPerPerson;
+	document.querySelector(".billAndTip").textContent = totalSum;
+}
